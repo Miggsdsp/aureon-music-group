4a55a1a527cb95bccf437d1531d672521d284a17
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-07-08 21:33:32 +0100

    Sprint 2: load artist page styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import './finish.css';
+import './sprint2.css';
 
 export const metadata: Metadata = {
   title: "Aureon Music Group | Creating Tomorrow's Classics",
