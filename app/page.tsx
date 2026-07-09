caa7b9436ba28a2517185b2a5e7fe0ee06420e80
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-07-09 21:59:22 +0100

    Remove footer from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { Header } from '@/components/Header';
 import { CinematicHero } from '@/components/CinematicHero';
-import { Footer } from '@/components/Footer';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <CinematicHero />
-      <Footer />
     </main>
   );
 }
