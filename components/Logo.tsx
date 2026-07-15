5bf8ca7f2eab65ba7e879855c2dc7504a69ebeb9
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-07-15 08:37:33 +0100

    Make site logo editable from CMS

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,17 +1,20 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePublishedDocument } from '@/lib/usePublishedDocument';
 
 export function Logo() {
+  const { data } = usePublishedDocument<any>('sitePages', 'header', {
+    logoUrl: '/images/branding/Aureon_Header_Logo.png',
+    logoAlt: 'Aureon Music Group'
+  });
+  const src = data?.logoUrl || '/images/branding/Aureon_Header_Logo.png';
+  const alt = data?.logoAlt || 'Aureon Music Group';
+
   return (
-    <Link href="/" className="logo aureon-image-logo" aria-label="Aureon Music Group home">
-      <Image
-        src="/images/branding/Aureon_Header_Logo.png"
-        alt="Aureon Music Group"
-        width={520}
-        height={180}
-        priority
-        className="aureon-logo-img"
-      />
+    <Link href="/" className="logo aureon-image-logo" aria-label={`${alt} home`}>
+      <Image src={src} alt={alt} width={520} height={180} priority className="aureon-logo-img" unoptimized />
     </Link>
   );
 }
