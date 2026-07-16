423499a3360e87fc65413075c74d679c99721ae4
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-07-16 22:56:30 +0100

    Add dynamic featured news and videos to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from '@/components/Header';
 import { CinematicHero } from '@/components/CinematicHero';
+import { HomeFeaturedContent } from '@/components/HomeFeaturedContent';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <CinematicHero />
+      <HomeFeaturedContent />
     </main>
   );
 }
