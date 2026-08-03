dd52d1b1402cbbcaebe3144e4fd75711f5afaa73
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-03 22:38:42 +0100

    Add recently played section to member account

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,5 +1,6 @@
 import { ContinueListening } from '@/components/discovery/ContinueListening';
+import { RecentlyPlayed } from '@/components/discovery/RecentlyPlayed';
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<ContinueListening /></>;
+  return <>{children}<ContinueListening /><RecentlyPlayed /></>;
 }
