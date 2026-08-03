a746b6950cf1322a1d7ec19c721de45eca9dd2df
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-03 22:49:52 +0100

    Add new releases to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { CinematicHero } from '@/components/CinematicHero';
 import { DeferredHomeFeaturedContent } from '@/components/DeferredHomeFeaturedContent';
 import { ContinueListening } from '@/components/discovery/ContinueListening';
 import { TrendingSongs } from '@/components/discovery/TrendingSongs';
+import { NewReleases } from '@/components/discovery/NewReleases';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <CinematicHero />
       <ContinueListening />
       <TrendingSongs compact />
+      <NewReleases compact showFilters={false} limit={8} />
       <DeferredHomeFeaturedContent />
     </main>
   );
