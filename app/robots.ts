ade79b052131a8c081b284738d15654b47a6dd93
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-03 21:33:36 +0100

    Reference XML, image and video sitemaps in robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,20 @@
 import type { MetadataRoute } from 'next';
-export default function robots():MetadataRoute.Robots{return{rules:[{userAgent:'*',allow:'/',disallow:['/admin','/api/']}],sitemap:'https://www.aureonmusicgroup.com/sitemap.xml',host:'https://www.aureonmusicgroup.com'};}
+import { SITE_URL } from '@/lib/seo';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/account/', '/checkout/', '/library/'],
+      },
+    ],
+    sitemap: [
+      `${SITE_URL}/sitemap.xml`,
+      `${SITE_URL}/image-sitemap.xml`,
+      `${SITE_URL}/video-sitemap.xml`,
+    ],
+    host: SITE_URL,
+  };
+}
