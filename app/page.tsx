3943b706b4b20367dce298b9b879fe25649e0919
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-03 22:33:41 +0100

    Show continue listening on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/Header';
 import { CinematicHero } from '@/components/CinematicHero';
 import { DeferredHomeFeaturedContent } from '@/components/DeferredHomeFeaturedContent';
+import { ContinueListening } from '@/components/discovery/ContinueListening';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <CinematicHero />
+      <ContinueListening />
       <DeferredHomeFeaturedContent />
     </main>
   );
