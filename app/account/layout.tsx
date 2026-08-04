cca36fd55aea32d82dc3d0d364b961ce7a011e3b
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-04 14:32:18 +0100

    Mount community dashboard in member account

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,8 +1,9 @@
+import { CommunityDashboard } from '@/components/community/CommunityDashboard';
 import { ContinueListening } from '@/components/discovery/ContinueListening';
 import { RecentlyPlayed } from '@/components/discovery/RecentlyPlayed';
 import { ReferralCapture } from '@/components/referrals/ReferralCapture';
 import { ReferralDashboard } from '@/components/referrals/ReferralDashboard';
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
-  return <><ReferralCapture />{children}<ReferralDashboard /><ContinueListening /><RecentlyPlayed /></>;
+  return <><ReferralCapture />{children}<CommunityDashboard /><ReferralDashboard /><ContinueListening /><RecentlyPlayed /></>;
 }
