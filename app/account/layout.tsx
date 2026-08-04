587833af56729267d07736c1b38defb3912b5e44
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-04 14:04:01 +0100

    Add referral dashboard to member account

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,6 +1,7 @@
 import { ContinueListening } from '@/components/discovery/ContinueListening';
 import { RecentlyPlayed } from '@/components/discovery/RecentlyPlayed';
+import { ReferralDashboard } from '@/components/referrals/ReferralDashboard';
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<ContinueListening /><RecentlyPlayed /></>;
+  return <>{children}<ReferralDashboard /><ContinueListening /><RecentlyPlayed /></>;
 }
