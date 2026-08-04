8d00569669117323d4efcbb84b305cf4d066ad5e
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-04 14:04:45 +0100

    Enable referral attribution on account routes

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,7 +1,8 @@
 import { ContinueListening } from '@/components/discovery/ContinueListening';
 import { RecentlyPlayed } from '@/components/discovery/RecentlyPlayed';
+import { ReferralCapture } from '@/components/referrals/ReferralCapture';
 import { ReferralDashboard } from '@/components/referrals/ReferralDashboard';
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<ReferralDashboard /><ContinueListening /><RecentlyPlayed /></>;
+  return <><ReferralCapture />{children}<ReferralDashboard /><ContinueListening /><RecentlyPlayed /></>;
 }
