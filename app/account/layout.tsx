94799c195fd3c440385c5d48cc56eb4f5f41c38b
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-05 09:27:25 +0100

    Mount account privacy and security trust layer

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -3,7 +3,8 @@ import { ContinueListening } from '@/components/discovery/ContinueListening';
 import { RecentlyPlayed } from '@/components/discovery/RecentlyPlayed';
 import { ReferralCapture } from '@/components/referrals/ReferralCapture';
 import { ReferralDashboard } from '@/components/referrals/ReferralDashboard';
+import { AccountTrustMount } from '@/components/trust/AccountTrustMount';
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
-  return <><ReferralCapture />{children}<CommunityDashboard /><ReferralDashboard /><ContinueListening /><RecentlyPlayed /></>;
+  return <><ReferralCapture />{children}<AccountTrustMount /><CommunityDashboard /><ReferralDashboard /><ContinueListening /><RecentlyPlayed /></>;
 }
