c1447dcad462ba5b715e7fcd916a14eb2f66a99c
Miggsdsp <103070042+Miggsdsp@users.noreply.github.com>
2026-08-18 07:43:27 +0100

    Fix Listener library mobile section overlap

--- components/member/ListenerExperienceMount.tsx
+++ components/member/ListenerExperienceMount.tsx
@@ -6,5 +6,5 @@ import ListenerExperience from './ListenerExperience';
 export default function ListenerExperienceMount() {
   const pathname = usePathname();
   if (pathname !== '/library') return null;
-  return <div style={{ maxWidth: '1600px', margin: '-110px auto 130px', padding: '0 5vw', position: 'relative', zIndex: 3 }}><ListenerExperience /></div>;
+  return <div className="listener-experience-mount"><ListenerExperience /></div>;
 }
